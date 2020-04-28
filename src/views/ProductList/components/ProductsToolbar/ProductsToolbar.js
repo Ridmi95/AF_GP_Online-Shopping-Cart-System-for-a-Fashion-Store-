822a2e0efcab04e016136cd2fc7f341f43fae00d
Gimhani <gimahaniash@gmail.com>
2020-04-29 00:02:56 +0530

    fourth commit

--- src/views/ProductList/components/ProductsToolbar/ProductsToolbar.js
+++ src/views/ProductList/components/ProductsToolbar/ProductsToolbar.js
@@ -1,66 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/styles';
-import { Button } from '@material-ui/core';
+import React, { Component } from 'react';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 
-import { SearchInput } from 'components';
+const dataProvider =
+  jsonServerProvider('https://jsonplaceholder.typicode.com');
 
-const useStyles = makeStyles(theme => ({
-  root: {},
-  row: {
-    height: '42px',
-    display: 'flex',
-    alignItems: 'center',
-    marginTop: theme.spacing(1)
-  },
-  spacer: {
-    flexGrow: 1
-  },
-  importButton: {
-    marginRight: theme.spacing(1)
-  },
-  exportButton: {
-    marginRight: theme.spacing(1)
-  },
-  searchInput: {
-    marginRight: theme.spacing(1)
+class ProductsToolbar extends Component {
+  render() {
+    return (
+      <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={ListGuesser} />
+      </Admin>
+    );
   }
-}));
-
-const ProductsToolbar = props => {
-  const { className, ...rest } = props;
-
-  const classes = useStyles();
-
-  return (
-    <div
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
-      <div className={classes.row}>
-        <span className={classes.spacer} />
-        <Button className={classes.importButton}>Import</Button>
-        <Button className={classes.exportButton}>Export</Button>
-        <Button
-          color="primary"
-          variant="contained"
-        >
-          Add product
-        </Button>
-      </div>
-      <div className={classes.row}>
-        <SearchInput
-          className={classes.searchInput}
-          placeholder="Search product"
-        />
-      </div>
-    </div>
-  );
-};
-
-ProductsToolbar.propTypes = {
-  className: PropTypes.string
-};
-
+}
 export default ProductsToolbar;
