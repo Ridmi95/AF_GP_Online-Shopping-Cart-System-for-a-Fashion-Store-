29a894041f9d17e00bffe3308ae9a6ed8f14bbd8
Ridmi95 <52588988+Ridmi95@users.noreply.github.com>
2020-05-11 00:38:42 +0530

    implement product and order routes

--- DB.js
+++ DB.js
@@ -1,5 +1,6 @@
 // DB.js
 
 module.exports = {
-  DB: 'mongodb://localhost:27017/Shopping_Cart'
-}
+  DB:
+    "mongodb+srv://root:0MKmrFE6ygcu2HGA@cluster0-njlqc.mongodb.net/shopping_cart",
+};
--- server.js
+++ server.js
@@ -12,12 +12,25 @@ const config = require('./DB.js');
 
 const categoryRoute = require('./Route/category.router');
 const loginRoute    = require('./Route/login.router');
+const productRoute = require("./Route/product.router");
+const orderRoute = require("./Route/order.router");
+
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-  () => {console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)}
-);
+mongoose
+  .connect(config.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(
+    () => {
+      console.log("Database is connected");
+    },
+    (err) => {
+      console.log("Can not connect to the database" + err);
+    }
+  );
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -25,6 +38,8 @@ app.use(bodyParser.json());
 
 app.use('/category', categoryRoute);
 app.use('/login', loginRoute);
+app.use("/product", productRoute);
+app.use("/order", orderRoute);
 
 app.listen(PORT, function(){
   console.log('Server is running on Port:',PORT);
