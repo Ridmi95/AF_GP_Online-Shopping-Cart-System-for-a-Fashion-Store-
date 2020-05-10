bb72d2602b36d632b3ac78ea74451700b3dc7a76
P D R Deshan <ravindudeshan9865@gmail.com>
2020-05-11 00:11:02 +0530

    reduce

--- README.md
+++
@@ -1,2 +0,0 @@
-# AF_GP_Online-Shopping-Cart-System-for-a-Fashion-Store-
-Online Shopping Cart System for a Fashion Store by using 1. HTML/JavaScript front end     2. ReactJS   3. NodeJS     4. ExpressJS     5. Spring Boot Framework     6. JSON base Web Services     7. NoSQL Database (MongoDB) 8. JEST and Back End from an API running JSON based web service and Database : MongoDB
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 let product = require('../models/product.model');
 
+
+//get all the products
 router.route('/').get((req, res) =>{
 
     product.find().then(products => res.json(products))
@@ -10,6 +12,16 @@ router.route('/').get((req, res) =>{
 
 });
 
+// get product by id
+router.route('/:id').get((req, res) =>{
+
+    product.findById(req.params.id).then(products => res.json(products))
+    .catch(err => res.status(400).json('Error: ' + err));
+
+
+});
+
+//add product
 router.route('/add').post((req, res) =>{
 
     const Name = req.body.Name;
@@ -22,6 +34,38 @@ router.route('/add').post((req, res) =>{
     const newProduct = new product({Name,Price,Color,Category,Discount,Qty});
 
     newProduct.save().then(()=> res.json('Product added!')).catch(err=> res.status(400).json('Erro' + err));
+
+
+});
+
+//delete product by ID
+router.route('/delete/:id').delete((req, res) =>{
+
+    product.findByIdAndDelete(req.params.id).then(products => res.json('products deleted'))
+    .catch(err => res.status(400).json('Error: ' + err));
+
+
+});
+
+//update product
+
+router.route('/update/:id').post((req, res) =>{
+
+    product.findById(req.params.id).then(
+
+        productObj=>{
+            productObj.Name = req.body.Name;
+            productObj.Price = req.body.Price;
+            productObj.Color = req.body.Color;
+            productObj.Category = req.body.Category;
+            productObj.Discount = req.body.Discount;
+            productObj.Qty = req.body.Qty;
+
+            productObj.save().then(()=> res.json('Updated')).catch(err => res.status(400).json('Erro ' + err));
+
+        }
+
+    ).catch(err => res.status(400).json('Error ' + err));
 
 
 });
