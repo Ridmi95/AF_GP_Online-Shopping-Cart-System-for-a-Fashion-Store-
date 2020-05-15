2da37a0af1e036dd603e9947fdd9475a286ad5dd
P D R Deshan <ravindudeshan9865@gmail.com>
2020-05-16 00:15:46 +0530

    commented order.route.js

--- Backend/Route/order.router.js
+++ Backend/Route/order.router.js
@@ -1,23 +1,23 @@
-"use strict";
-const express = require("express");
-const orderRoutes = express.Router();
+// "use strict";
+// const express = require("express");
+// const orderRoutes = express.Router();
 
-let Order = require("../Models/order.model");
+// let Order = require("../Models/order.model");
 
-orderRoutes.route("/add").post(function (req, res) {
-  let orders = new Order(req.body);
+// orderRoutes.route("/add").post(function (req, res) {
+//   let orders = new Order(req.body);
 
-  orders
-    .save()
-    .then((order) => {
-      res.status(200).json({ success: true, data: order });
-    })
-    .catch((err) => {
-      res.status(400).json({ success: false, data: err });
-    });
-});
+//   orders
+//     .save()
+//     .then((order) => {
+//       res.status(200).json({ success: true, data: order });
+//     })
+//     .catch((err) => {
+//       res.status(400).json({ success: false, data: err });
+//     });
+// });
 
-module.exports = orderRoutes;
+// module.exports = orderRoutes;
 "use strict";
 const express = require("express");
 const orderRoutes = express.Router();
