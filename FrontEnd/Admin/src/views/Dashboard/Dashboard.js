6676d0e0efc3b05a6cc2e2eef0181e6c5315b7f1
Gimhani <gimahaniash@gmail.com>
2020-05-22 19:01:12 +0530

    final commit - dashboard

--- FrontEnd/Admin/src/views/Dashboard/Dashboard.js
+++ FrontEnd/Admin/src/views/Dashboard/Dashboard.js
@@ -8,6 +8,9 @@ import {
   TasksProgress,
   TotalProfit,
   LatestProducts,
+  LatestSales,
+  UsersByDevice
+
   
 } from './components';
 import {Card} from 'react-bootstrap';
@@ -88,7 +91,25 @@ const Dashboard = () => {
           <LatestProducts />
         </Grid>
 
+        <Grid
+          item
+          lg={8}
+          md={12}
+          xl={9}
+          xs={12}
+        >
+          <LatestSales />
+        </Grid>
 
+        <Grid
+          item
+          lg={4}
+          md={6}
+          xl={3}
+          xs={12}
+        >
+          <UsersByDevice />
+        </Grid>
 
 
 
