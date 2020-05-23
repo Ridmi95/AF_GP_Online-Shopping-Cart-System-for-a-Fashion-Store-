535addf16d7839edd8896ce82316577f72706ef8
Ridmi95 <52588988+Ridmi95@users.noreply.github.com>
2020-05-23 08:31:22 +0530

    Modified

--- FrontEnd/Client/src/action/cart.js
+++ FrontEnd/Client/src/action/cart.js
@@ -1,6 +1,7 @@
 // @flow
 import Alert from "components/Alert";
 
+//notificationHandeler
 export function notificationHandler(isSuccess, message) {
   return {
     type: "CART_HANDLE_NOTIFICATIONS",
@@ -14,26 +15,31 @@ export function notificationHandler(isSuccess, message) {
   };
 }
 
+//addtocart
 export function addToCart(payload) {
   return (dispatch) => {
     dispatch({ type: "ADD_TO_CART_SUCCESS", payload });
   };
 }
 
+//removeFromCart
 export function removeFromCart(productCode) {
   return (dispatch) => {
     dispatch({ type: "REMOVE_FROM_CART_SUCCESS", payload: { productCode } });
   };
 }
 
+//editCartItem
 export function editCartItems(payload) {
   return (dispatch) => {
     dispatch({ type: "EDIT_CART_SUCCESS", payload });
   };
 }
 
+//handleCart
 export function handleCart() {
   return (dispatch) => {
     dispatch({ type: "HANDLE_CART_SUCCESS" });
   };
+  
 }
